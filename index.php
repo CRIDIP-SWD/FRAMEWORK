47c424dc6b1e25514bdd8a047e3147c9e8fe336f
CRIDIP-SWD <mmockelyn@cridip.com>
2016-05-31 01:59:04 +0200

    AUTOLOAD

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maxime
- * Date: 31/05/2016
- * Time: 01:33
- */
+//session_start();
+
+require "app/classe.php";
+
+if(isset($_GET['view'])){
+    $view = $_GET['view'];
+}else{
+    $view = "index";
+}
+
+ob_start();
+
+//START ROUTE
+
+if($view === 'index'){require "view/index.php";}
+
+$content = ob_get_clean();
+
+require "view/template.php";
+
+// END ROUTE
